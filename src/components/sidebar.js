d56867e994d04bce3bbe0c5a8a9a19bcb13adb2c
Devang Pant <d3pant@ucsd.edu>
2025-09-09 22:23:51 +0530

    second

---
+++ src/components/sidebar.js
@@ -0,0 +1,22 @@
+import 'sidebar.css'
+export default function Sidebar(){
+    return(
+        <nav className="Side">
+            <div className="left-align">
+                <Bttn text="CHK"/>
+            </div>
+            <div className="right-align">
+                <Bttn text="CHK"/>
+                <Bttn text="CHK"/>
+                <Bttn text="CHK"/>
+            </div>
+        </nav>
+    );
+}
+function Bttn({text}){
+    return(
+        <button className='Button'>
+        {text}
+        </button>
+    );
+}
